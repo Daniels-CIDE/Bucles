57c241e92462416fb9905c15d058ef02afca95c5
Daniels-CIDE <Daniels-CIDE@github.com>
2021-02-03 19:42:15 +0100

    Finish

--- src/bingo.java
+++ src/bingo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by: Daniels
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 public class bingo {
     public ArrayList<Integer> carton1 = new ArrayList<>();
     public ArrayList<Integer> carton2 = new ArrayList<>();
+    boolean salir = false;
 
     public static void main(String[] args) {
         bingo vi = new bingo();
@@ -21,18 +23,48 @@ public class bingo {
             carton1.add((int) ((Math.random() * 99) + 1));
             carton2.add((int) ((Math.random() * 99) + 1));
         }
-        System.out.println(carton1);
-        System.out.print(carton2);
+        //System.out.println(carton1);
+        //System.out.println(carton2);
     }
 
     public void numerosBingo () {
+        ArrayList<Integer> insideBingo = new ArrayList<>();
+        for (int i = 1; i < 100; ++i) {
+            insideBingo.add(i);
+        }
+        //System.out.print(insideBingo);
 
+        while (!salir){
+            int numeroBingo = (int) ((Math.random() * 99) + 1);
+            if (insideBingo.contains(numeroBingo)) {
+                insideBingo.removeAll(Arrays.asList(numeroBingo));
+
+                if (carton1.contains(numeroBingo)) {
+                    carton1.removeAll(Arrays.asList(numeroBingo));
+                    System.out.println("Jugador 1 tiene el numero " + numeroBingo);
+                } else
+                    System.out.println("Jugador 1 no tiene el numero: " + numeroBingo);
+
+
+                if (carton2.contains(numeroBingo)) {
+                    carton2.removeAll(Arrays.asList(numeroBingo));
+                    System.out.println("Jugador 2 tiene el numero " + numeroBingo);
+
+                } else
+                    System.out.println("Jugador 2 no tiene el numero: " + numeroBingo);
+            }
+            comprovarCarton();
+
+        }
     }
 
     public void comprovarCarton() {
-        if (carton1.isEmpty())
+        if (carton1.isEmpty()) {
             System.out.print("Bigooooo!!!! del Jugador 1");
-        else if (carton2.isEmpty())
+            salir = true;
+        }else if (carton2.isEmpty()){
             System.out.print("Bigooooo!!!! del Jugador 2");
+            salir=true;
+        }
     }
 }
